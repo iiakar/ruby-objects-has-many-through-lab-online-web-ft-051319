f303eadac593f730ae25a17589c08aa2120bcf59
iiakar <ibsakar>
2020-02-03 16:03:46 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,26 @@
+class Doctor
+    attr_accessor :name, :appointments, :patient
+      @@all=[]
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+     appointment = Appointment.new(patient, date, self)
+     @appointments << appointment
+     appointment
+  end
+
+  def patients
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+end
